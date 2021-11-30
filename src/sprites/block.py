180c09e40b72da340c8691715adfb15fd25727fd
Essi Prykäri <essi.prykari@helsinki.fi>
2021-11-30 18:11:47 +0200

    change variable names & lint

--- src/sprites/block.py
+++ src/sprites/block.py
@@ -3,13 +3,13 @@ from image_loader import load_image
 
 class Block(pygame.sprite.Sprite):
 
-    def __init__(self, x, y):
+    def __init__(self, x_coordinate, y_coordinate):
         super().__init__()
 
         self.name = "placeholder"
         self.image = load_image("block.png")
         self.rect = self.image.get_rect()
 
-        self.rect.y = y
-        self.rect.x = x
+        self.rect.x = x_coordinate
+        self.rect.y = y_coordinate
         self.rotation = "placeholder"
