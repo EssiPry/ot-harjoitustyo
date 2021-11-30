b84ec4a105cc2b622a6eb399bf2dff6955186453
Essi Prykäri <essi.prykari@helsinki.fi>
2021-11-30 14:48:17 +0200

    update index.py

--- src/index.py
+++ src/index.py
@@ -1,45 +1,76 @@
 import pygame
+from level import Level
+from sprites.block import Block
+
+LEVEL_MAP = [[1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,0,0,0,0,0,0,0,0,0,0,1],
+             [1,1,1,1,1,1,1,1,1,1,1,1],
+             ]
+
+CELL_SIZE = 30
 
 def main():
-    pygame.init()
-    game_display = pygame.display.set_mode((300, 600))
-    pygame.display.set_caption('Testi-tetris')
+    level_map = LEVEL_MAP
+    level_height = CELL_SIZE * len(level_map)
+    level_width = CELL_SIZE * len(level_map[1])
 
-    x = 120
-    y = 00
-    width = 60
-    height = 60
-    vel = 30
+    display = pygame.display.set_mode((level_width, level_height))
+    pygame.display.set_caption("Block-Tetris")
+
+    display.fill((0,0,0))
+
+    pygame.init()
+
+    level = Level(level_map, CELL_SIZE)
+    level.all_sprites.draw(display)
+    pygame.display.update()
+
+    level._new_block()
+    level.all_sprites.draw(display)
 
     clock = pygame.time.Clock()
 
     run = True
 
     while run:
-        pygame.time.delay(100)
-
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
-                exit()
+                run = False
 
-        keys = pygame.key.get_pressed()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    level._block_move("left")
+                elif event.key == pygame.K_RIGHT:
+                    level._block_move("right")
+                elif event.key == pygame.K_UP:
+                    print('rotate')
 
-        if keys[pygame.K_LEFT] and x >= vel:
-            x -= vel
+        level._block_fall()
+        level.all_sprites.draw(display)
+        pygame.display.update()
 
-        if keys[pygame.K_RIGHT] and x < 300 - width:
-            x += vel
+        clock.tick(30)
 
-        if keys[pygame.K_DOWN] and y <= 600 - height -vel:
-            y += vel
-
-        game_display.fill((0,0,0))
-        pygame.draw.rect(game_display,(255, 134, 94), (x, y, width, height))
-        pygame.display.flip()
-        if y <= 600 - height:
-            y += 1
-        clock.tick(60)
-
+    exit()
 
 if __name__== "__main__":
     main()
