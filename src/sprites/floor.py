5b63d3a448c25fd71347fd0ae1ac97ddb4767f43
Essi Prykäri <essi.prykari@helsinki.fi>
2021-12-05 13:01:12 +0200

    Add floor & top sprite classes

---
+++ src/sprites/floor.py
@@ -0,0 +1,13 @@
+import pygame
+from image_loader import load_image
+
+class Floor(pygame.sprite.Sprite):
+
+    def __init__(self, x_coordinate=0, y_coordinate=0):
+        super().__init__()
+
+        self.image = load_image("wall.png")
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x_coordinate
+        self.rect.y = y_coordinate
