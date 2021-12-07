b5eeb54adefb5af8150cca4921621f495fec6da9
Essi Prykäri <essi.prykari@helsinki.fi>
2021-12-07 18:42:24 +0200

    add block move tests

--- src/tests/level_test.py
+++ src/tests/level_test.py
@@ -1,26 +1,47 @@
 import unittest
 
 from level import Level
+from sprites.block import Block
 
-LEVEL_MAP = [[1,1,1,1,1,1,1],
-             [1,0,0,0,0,0,1],
-             [1,0,0,0,0,0,1],
-             [1,0,0,0,0,0,1],
-             [1,0,0,0,0,0,1],
-             [1,0,0,0,0,0,1],
-             [1,0,0,0,0,0,1],
-             [1,0,0,0,0,0,1],
-             [1,0,0,0,0,0,1],
-             [1,0,0,0,0,0,1],
-             [1,0,0,0,0,0,1],
+
+LEVEL_MAP = [[3,3,3,3,3,3,3],
+             [2,0,0,0,0,0,2],
+             [2,0,0,0,0,0,2],
+             [2,0,0,0,0,0,2],
+             [2,0,0,0,0,0,2],
+             [2,0,0,0,0,0,2],
+             [2,0,0,0,0,0,2],
+             [2,0,0,0,0,0,2],
+             [2,0,0,0,0,0,2],
+             [2,0,0,0,0,0,2],
+             [2,0,0,0,0,0,2],
              [1,1,1,1,1,1,1]]
 
 CELL_SIZE = 30
 
 class TestLevel(unittest.TestCase):
     def setUp(self):
-        self.level = Level(LEVEL_MAP, CELL_SIZE)
+        self.test_level = Level(LEVEL_MAP, CELL_SIZE)
+        self.test_level.block = Block(CELL_SIZE*3, CELL_SIZE)
 
-    def test_new_block(self):
-        self.level._new_block()
-        self.assertIsNotNone(self.level.block)
+    def test_new_block_exists(self):
+        self.test_level._new_block()
+        self.assertIsNotNone(self.test_level.block)
+
+    def assert_coordinates_equal(self, sprite, x, y):
+        self.assertEqual(sprite.rect.x, x)
+        self.assertEqual(sprite.rect.y, y)
+
+    def test_block_move_left(self):
+        block = self.test_level.block
+        self.assert_coordinates_equal(block, CELL_SIZE*3, CELL_SIZE)
+
+        self.test_level._block_move("left")
+        self.assert_coordinates_equal(block, CELL_SIZE*2, CELL_SIZE)
+
+    def test_block_move_right(self):
+        block = self.test_level.block
+        self.assert_coordinates_equal(block, CELL_SIZE*3, CELL_SIZE)
+
+        self.test_level._block_move("right")
+        self.assert_coordinates_equal(block, CELL_SIZE*4, CELL_SIZE)
