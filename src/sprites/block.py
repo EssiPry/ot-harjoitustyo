3c616d12fed51e46fcbdf6f84ea63baa27ff8287
Essi Prykäri <essi.prykari@helsinki.fi>
2021-12-07 18:44:52 +0200

    update block class

--- src/sprites/block.py
+++ src/sprites/block.py
@@ -6,10 +6,10 @@ class Block(pygame.sprite.Sprite):
     def __init__(self, x_coordinate, y_coordinate):
         super().__init__()
 
-        self.name = "placeholder"
+        self.name = "placeholder for name"
         self.image = load_image("block.png")
         self.rect = self.image.get_rect()
 
         self.rect.x = x_coordinate
         self.rect.y = y_coordinate
-        self.rotation = "placeholder"
+        self.rotation = "placeholder for rotation"
