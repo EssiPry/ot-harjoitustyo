670af25d8ecfb20f88fb85d26d6847f8571c19df
Essi Prykäri <essi.prykari@helsinki.fi>
2021-12-14 16:38:02 +0200

    add unit tests to level and shape -classes

---
+++ src/tests/level_test.py
@@ -0,0 +1,35 @@
+import unittest
+from level import Level
+from shape import Shape
+
+class TestLevel(unittest.TestCase):
+    def setUp(self):
+        self.test_level = Level(30)
+        self.test_shape = Shape()
+
+    def test_level_ctor(self):
+        self.assertEqual(len(self.test_level.matrix), 20)
+        self.assertEqual(self.test_level.block_size, 30)
+
+    def test_add_shape_in_matrix(self):
+        self.test_level.add_shape_in_matrix(self.test_shape)
+        self.assertEqual(self.test_level.matrix[0][4], 'I')
+        self.assertEqual(self.test_level.matrix[1][4], 'I')
+        self.assertEqual(self.test_level.matrix[2][4], 'I')
+        self.assertEqual(self.test_level.matrix[3][4], 'I')
+
+    def test_erase_shape_from_matrix(self):
+        self.test_level.add_shape_in_matrix(self.test_shape)
+        self.assertEqual(self.test_level.matrix[0][4], 'I')
+        self.test_level.erase_shape_from_matrix(self.test_shape)
+        self.assertEqual(self.test_level.matrix[0][4], '.')
+
+    def test_check_for_full_rows(self):
+        for i in range(10):
+            self.test_level.matrix[5][i] = 'T'
+        self.assertEqual(self.test_level.check_for_full_rows(),[5])
+
+    def test_check_game_over(self):
+        self.assertFalse(self.test_level.check_game_over())
+        self.test_level.matrix[0][5] = 'B'
+        self.assertTrue(self.test_level.check_game_over())
---
+++ src/tests/shape_test.py
@@ -0,0 +1,35 @@
+import unittest
+from shape import Shape
+from level import Level
+
+class TestShape(unittest.TestCase):
+    def setUp(self):
+        self.test_shape = Shape()
+        self.test_level = Level(25)
+
+    def test_shape_ctor(self):
+        self.assertEqual(self.test_shape.name, 'I')
+        self.assertEqual(self.test_shape.coordinates,[[0, 4], [1, 4], [2, 4], [3, 4]])
+        self.assertEqual(self.test_shape.rotation, 0)
+        self.assertEqual(self.test_shape.colour,(145,69,182))
+        self.assertFalse(self.test_shape.locked)
+
+    def test_move_shape_left(self):
+        self.test_shape.move_shape('left', self.test_level)
+        self.assertEqual(self.test_shape.coordinates, [[0, 3], [1, 3], [2, 3], [3, 3]])
+
+    def test_move_shape_right(self):
+        self.test_shape.move_shape('right', self.test_level)
+        self.assertEqual(self.test_shape.coordinates, [[0, 5], [1, 5], [2, 5], [3, 5]])
+
+    def test_check_no_collisions(self):
+        self.assertTrue(self.test_shape.check_no_collisions(self.test_level))
+
+    def test_check_no_collisions_when_there_is_a_collision(self):
+        self.test_shape.coordinates = [[17, 4], [18, 4], [19, 4], [20, 4]]
+        self.assertFalse(self.test_shape.check_no_collisions(self.test_level))
+
+    def test_lock_shape(self):
+        self.test_shape.lock_shape()
+        self.assertTrue(self.test_shape.locked)
+        self.assertEqual(self.test_shape.name, 'B')
